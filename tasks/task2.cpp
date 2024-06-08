a70a5f01d174b815e03dd54d84160f3c51a74fba
onsntt <onsnt@mail.ru>
2024-06-08 18:57:15 +0500

    task2 ready

--- tasks/task2.cpp
+++ tasks/task2.cpp
@@ -1,108 +1,192 @@
 #include <iostream>
 #include <vector>
+enum room_type
+{
+    living = 1,
+    children = 2,
+    bedroom = 3,
+    kitchen = 4,
+    bathroom = 5
+};
 
 struct room
 {
-    bool roomExist = 0;
-    enum roomType
-    {
-        none,
-        bedroom,
-        kitchen,
-        bathroom,
-        childrenroom,
-        livingroom
-    };
-    double roomSize;
+    double area;
+    room_type type;
+};
+struct store
+{
+    double height;
+    std::vector<room> rooms;
 };
 
-struct houseFloor
+enum building_type
 {
-    int floorNumber;
-    int roomCount;
-    double floorHeight = 2.5;
-    enum floorType
-    {
-        none,
-        first,
-        second,
-        third
-    };
-    enum rooms
-    {
-        room1,
-        room2,
-        room3,
-        room4
-    };
+    house = 1,
+    garage = 2,
+    barn = 3,
+    bath = 4
 };
 
-struct house
+struct building
 {
-    bool houseExist = 0;
-    int floorCount;
-    double houseSize;
-    houseFloor firstFloor;
-    houseFloor secondFloor;
-    houseFloor thirdFloor;
-    bool furnace = 0;
-};
-
-struct outbuilding
-{
-    bool obExist = 0;
-    enum outbuildingType
-    {
-        none,
-        bathhouse,
-        garage,
-        barn
-    };
-
-    double outbuildingSize;
-};
-
-struct bathhouse
-{
-    bool furnace = 0;
+    bool has_chimney;
+    building_type type;
+    double area;
+    std::vector<store> stores;
 };
 
 struct plot
 {
     int number;
-    house house1;
-    outbuilding ob1;
-    outbuilding ob2;
-    outbuilding ob3;
+    double area;
+    std::vector<building> buildings;
 };
 
-struct town
+struct village
 {
-  std::string name = "sometown";
-  int plotCount = 0;
-
+    std::vector<plot> plots;
 };
 
+std::string bType(building_type x)
+{
+    std::string str;
+    switch(x)
+      {
+      case 1:
+          str = "House";
+          break;
+
+      case 2:
+          str = "Garage";
+          break;
+
+      case 3:
+          str = "Barn";
+          break;
+
+      case 4:
+          str = "Bath";
+          break;
+      }
+    return str;
 
 
 
+}
+std::string rType(room_type x)
+{
+    std::string str;
+    switch (x)
+    {
+    case 1:
+        str = "Living room";
+        break;
 
+    case 2:
+        str = "Children room";
+        break;
 
+    case 3:
+        str = "Bedroom";
+        break;
 
+    case 4:
+        str = "Kitchen";
+        break;
 
+    case 5:
+        str = "Bathroom";
+        break;    
+    }
+    return str;
+}
 
+int main()
+{
+    int plotCount = rand()%100 + 10;
+    
+    
+    village v;
+    for (int i = 0; i < plotCount; i++)
+    {
+        plot p;
+        p.number = i + 1;
+        p.area = rand()%5 + 11;
+        int buildingCount = rand()%4 +1;
+       
+        for (int j = 0; j < buildingCount; j++)
+        {
+            building b;
+            b.type = building_type(rand()%4 + 1);
+            
+            b.area = rand()%100 + 80;
+            int storeCount;
+            if (b.type == house || b.type == bath) 
+            {
+                b.has_chimney = rand()%2;
+            } else
+                b.has_chimney = 0;
+                    if (b.type == house)
+                {
+                    storeCount = rand() % 3 + 1;
+            }
+            else
+            {
+                storeCount = 1;
+            }
+            
+            for (int l = 0; l < storeCount; l++)
+            {
+                store s;
+                s.height = rand()%11 + 250;
+                int roomCount;
+                if(b.type == house) 
+                  {
+                      roomCount = rand() % 3 + 2;
+                  }
+                else
+                  {
+                    roomCount = 0;
+                  } 
 
-   
+                for (int w = 0; w < roomCount; w++)
+                {
+                    room r;
+                    r.area = rand()%10 + 12;
+                    r.type = room_type(rand()%5 + 1);
+                    s.rooms.push_back(r);
+                }
+                b.stores.push_back(s);
+            }
+            p.buildings.push_back(b);
+        }
+        v.plots.push_back(p);
+    }
+    for (int i = 0; i < plotCount; i++)
+    {
+        std::cout << "\nPlot number - " << v.plots[i].number << "\n";
+        std::cout << "Plot " << v.plots[i].number << " area is " << v.plots[i].area << "\n";
+        std::cout << "There are " << v.plots[i].buildings.size() << " buildings on this plot\n";
 
-
-
-int main() {
-
-room r1;
-houseFloor::
-
-
-
-
-
+        for (int j = 0; j < v.plots[i].buildings.size(); j++)
+        {
+             
+             std::cout << "\tBuilding " << bType(v.plots[i].buildings[j].type) << "\n";
+             std::cout << "\t" << bType(v.plots[i].buildings[j].type) << " area is " << v.plots[i].buildings[j].area << "\n";
+             std::cout << "\tThis building has " <<  v.plots[i].buildings[j].stores.size() << " stores \n";
+             if (v.plots[i].buildings[j].has_chimney == 1) std::cout << "\t There is a chimney in this building \n";
+                 for (int l = 0; l < v.plots[i].buildings[j].stores.size(); l++)
+                 {
+                     std::cout << "\t" << "\t Store " << l + 1 << " has " << v.plots[i].buildings[j].stores[l].rooms.size() << " rooms.\n";
+                     std::cout << "\t" << "\t The height of this store is " << v.plots[i].buildings[j].stores[l].height << " \n";
+                     for (int w = 0; w < v.plots[i].buildings[j].stores[l].rooms.size(); w++)
+                     {
+                         std::cout << "\t"<< "\t"<< "\t" << "Room " << w + 1 << " is a " << rType(v.plots[i].buildings[j].stores[l].rooms[w].type) << "\n";
+                         std::cout << "\t" << "\t" << "\t" << rType(v.plots[i].buildings[j].stores[l].rooms[w].type) << " area is " << v.plots[i].buildings[j].stores[l].rooms[w].area << "\n";
+                     }
+                 }
+        }
+    }
+    
 }
